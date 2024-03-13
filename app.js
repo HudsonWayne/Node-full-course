7365e01cbc01d5bdf3071eb5f467c4a7095ecd8a
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-13 15:52:20 +0200

    first dayy of node.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const amount = 12
+// const amount = 12
 
-if(amount < 10){
-    console.log('small number');
-}else{
-    console.log('large number');
-}
-console.log('hey its my first node app.');
+// if(amount < 10){
+//     console.log('small number');
+// }else{
+//     console.log('large number');
+// }
+// console.log('hey its my first node app.');
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('hello world')
+}, 3)
