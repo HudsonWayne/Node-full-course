75d5078dd2a726baa69a8039ec52aa8e78b0f611
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-14 09:39:17 +0200

    splice method

--- app.js
+++ app.js
@@ -12,17 +12,32 @@
 //     console.log('hello world')
 // }, 300)
 
-JavaScript
-function reverseString(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-}
+// JavaScript
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
 
-const originalString = "Hello, world!";
-const reversedString = reverseString(originalString);
+// const originalString = "Hello, world!";
+// const reversedString = reverseString(originalString);
 
-console.log("Original:", originalString);
-console.log("Reversed:", reversedString); // !dlrow ,olleH
+// console.log("Original:", originalString);
+// console.log("Reversed:", reversedString); // !dlrow ,olleH
+
+// const john = 'john';
+// const peter = 'peter';
+
+// const sayHi = (name) => {
+//     console.log(`Hello there  ${name}`)
+// }
+
+// sayHi('susan')
+// sayHi(john)
+// sayHi(peter)
+
+const numbers = [1,2,3,4,5];
+numbers.splice(2, 3);
+console.log(numbers)
