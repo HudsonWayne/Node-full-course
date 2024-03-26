4b29f25ad04f162cd25154edb55570873bb5306b
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-26 13:42:01 +0200

    updated the app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const{readFileSync,writeFileSync} = require('fs')
-
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const{readFile,writeFile} = require('fs')
 
 
-writeFileSync(
-    './content/result-sync.txt', 
-`Here is the result : ${first}, ${second}`, 
-{flag: 'a'}
-)
+readFile('./content/first.txt','utf8', (err, result)=> {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8',(err,result)=>{
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile('/content/result-async.txt')
+    })
+})
